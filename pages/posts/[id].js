3e6d5178293128696941aa31bbd693ff30dde6bb
Jeffrey Bentley <bentleybuilds@gmail.com>
2022-09-22 21:23:00 -0600

    Implemeted utils for static paths, static props

---
+++ pages/posts/[id].js
@@ -0,0 +1,36 @@
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+import Layout from '../../components/layout';
+
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+      </Layout>
+    );
+  }
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+      paths,
+      fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the blog post using params.id
+}
